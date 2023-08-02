29e3df233d0866b485fb8f8b82de9b7064c5119c
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2023-08-01 19:17:21 -0500

    add domain code getmovie by id

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -49,5 +50,16 @@ class MoviedbDatasource extends MoviesDataSource {
     final response =
         await dio.get("/movie/top_rated", queryParameters: {'page': page});
     return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get("/movie/$id");
+    if (response.statusCode != 200)
+      throw Exception("Movie con ID $id no encontrado");
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+    return movie;
   }
 }
